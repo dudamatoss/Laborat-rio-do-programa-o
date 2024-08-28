14d5f058750911f9f48eee724e8770f30d924b96
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2024-08-27 21:34:50 -0300

    calculadora
    c#

---
+++ calculadora/Calculadora.cs
@@ -0,0 +1,23 @@
+
+
+class Calculadora
+{
+    public double Somar(double x, double y)
+    {
+      double resultado = x + y ; 
+      return resultado;
+    }
+    public double Subtrair (double x, double y)
+    {
+        return x - y;
+    }
+    public double Multiplicacao (double x, double y)
+    {
+        return x * y;
+    }
+    public double Dividir(double x, double y)
+    {
+        return x / y;
+    }
+
+}
---
+++ calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿Calculadora calc = new Calculadora();
+Console.WriteLine("Informe um valor para x:");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe um valor para y:");
+double y = Convert.ToDouble(Console.ReadLine());
+
+
+double resultadoSoma = calc.Somar(x,y);
+Console.WriteLine($"O resultado da soma enter {x} e {y} é : {resultadoSoma}");
+
+Console.WriteLine($"O resultado da subtração é: {calc.Subtrair(x,y)} ");
+
+Console.WriteLine($"O resultado da multiplicação é: {calc.Multiplicacao(x,y)} ");
+
+Console.WriteLine($"O resultado da divisão é: {calc.Dividir(x,y)} ");
