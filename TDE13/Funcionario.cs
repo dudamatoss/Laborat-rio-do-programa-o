104a00ba9fb395fe50084fdc74f8538ffeaacec8
dudamatoss <164113930+dudamatoss@users.noreply.github.com>
2024-10-08 21:36:11 -0300

    tde 13

---
+++ TDE13/Funcionario.cs
@@ -0,0 +1,16 @@
+public class Funcionario
+{
+    public string Nome{ get ; set; }
+    public int Idade{get; set;}
+    public virtual double CalcularSalario()
+    {
+        return 1000.0;
+    }
+
+    public Funcionario(string nome, int idade)
+    {
+        Nome = nome;
+        Idade =idade;
+    }
+    
+}
---
+++ TDE13/Program.cs
@@ -0,0 +1,11 @@
+﻿Empresa empresa = new Empresa();
+Funcionario f1 = new Funcionario("Eduarda", 19);
+Gerente gerente = new Gerente("Maria", 57, 5000.0);
+Estagiario estagiario = new Estagiario("Laura", 16);
+
+empresa.AdicionarFuncionario(f1);
+empresa.AdicionarFuncionario(gerente);
+empresa.AdicionarFuncionario(estagiario);
+
+empresa.ListarFuncionarios();
+
